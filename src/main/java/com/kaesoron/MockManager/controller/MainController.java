ef3be09808cd115b289998c37eae8c3ffc705e4c
Kaesoron <postal0055@gmail.com>
2024-01-02 18:16:01 +0300

    Added endpoints

--- src/main/java/com/kaesoron/MockManager/controller/MainController.java
+++ src/main/java/com/kaesoron/MockManager/controller/MainController.java
@@ -3,7 +3,6 @@ package com.kaesoron.MockManager.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kaesoron.MockManager.model.Endpoint;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
