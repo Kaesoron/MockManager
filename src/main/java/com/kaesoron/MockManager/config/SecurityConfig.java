2d907b490d30288311085ed316447878a8dc6586
Kaesoron <postal0055@gmail.com>
2024-01-04 21:22:31 +0300

    Basic auth added

---
+++ src/main/java/com/kaesoron/MockManager/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.kaesoron.MockManager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public DefaultSecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
