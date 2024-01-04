9d71bef90c50204c329fa571988a642fd82d9782
Kaesoron <postal0055@gmail.com>
2024-01-04 13:21:15 +0300

    JSON CRUD operations

--- src/main/java/com/kaesoron/MockManager/controller/MainController.java
+++ src/main/java/com/kaesoron/MockManager/controller/MainController.java
@@ -1,26 +1,69 @@
 package com.kaesoron.MockManager.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kaesoron.MockManager.model.Endpoint;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("")
 public class MainController {
     private List<Endpoint> endpoints;
+    private File endpointsToEdit = new File("src/main/resources/endpoints.json");
+    ObjectMapper mapper = new ObjectMapper();
+
 
     public MainController() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
         TypeReference<List<Endpoint>> typeReference = new TypeReference<>() {};
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("endpoints.json");
         endpoints = mapper.readValue(inputStream, typeReference);
+
+    }
+
+    @GetMapping("/settings")
+    public ResponseEntity<String> getEndpoints() {
+        try {
+            JsonNode jsonNode = mapper.readTree(endpointsToEdit);
+            return ResponseEntity.ok(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Error reading endpoints list");
+        }
+    }
+
+    @PostMapping("/settings")
+    @PutMapping("/settings")
+    public ResponseEntity<String> addEndpoint(@RequestBody Endpoint endpoint) {
+        try {
+            List<Endpoint> endpoints = mapper.readValue(endpointsToEdit, new TypeReference<>(){});
+            endpoints.add(endpoint);
+            mapper.writeValue(endpointsToEdit, endpoints);
+            return ResponseEntity.ok("Endpoint added successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Error adding endpoint");
+        }
+    }
+
+    @DeleteMapping("/settings")
+    public ResponseEntity<String> deleteEndpoint(@RequestBody Endpoint endpointToBeDeleted) {
+        try {
+            List<Endpoint> endpoints = mapper.readValue(endpointsToEdit, new TypeReference<>(){});
+            endpoints.remove(endpointToBeDeleted);
+            mapper.writerWithDefaultPrettyPrinter().writeValue(endpointsToEdit, endpoints);
+            return ResponseEntity.ok("Endpoint deleted successfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("Error deleting endpoint");
+        }
     }
 
     @GetMapping("/**")
