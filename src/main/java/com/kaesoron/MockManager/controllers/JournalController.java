038a29ba7d19ad4acb5964311e7c6f9fddc1eefe
Kaesoron <postal0055@gmail.com>
2024-04-21 23:03:24 +0300

    Sorting

--- src/main/java/com/kaesoron/MockManager/controllers/JournalController.java
+++ src/main/java/com/kaesoron/MockManager/controllers/JournalController.java
@@ -5,6 +5,7 @@ import com.kaesoron.MockManager.models.Journal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
-import java.util.List;
 
 @Controller
 @RequestMapping("/journal")
@@ -29,8 +29,10 @@ public class JournalController {
                                   @RequestParam(name = "method", required = false) String method,
                                   @RequestParam(name = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date date,
                                   @RequestParam(name = "page", defaultValue = "0") int page,
-                                  @RequestParam(name = "size", defaultValue = "10") int size) {
-        Page<Journal> journals = journalDAO.search(mockName, mockId, method, date, PageRequest.of(page, size, Sort.by("journalDateTime").descending()));
+                                  @RequestParam(name = "size", defaultValue = "10") int size,
+                                  @RequestParam(name = "sort", defaultValue = "journalDateTime") String sort) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort).descending());
+        Page<Journal> journals = journalDAO.search(mockName, mockId, method, date, pageable);
         model.addAttribute("journals", journals);
         return "journal";
     }
